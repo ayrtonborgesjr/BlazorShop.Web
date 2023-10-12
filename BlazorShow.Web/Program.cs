9b2d5169cee9bdb963c94c8d21805516717bd1ab
ayrtonborgesjr <ayrton.borges@yahoo.com.br>
2023-10-12 13:43:20 -0300

    Nova migration e script para popular as tabelas

--- BlazorShow.Web/Program.cs
+++ BlazorShow.Web/Program.cs
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var baseUrl = "https://localhost:7269";
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseUrl) });
 
 await builder.Build().RunAsync();
